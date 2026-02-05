8b03b89059853c28d52b78e664a4864d0781e527
akashtiwari30 <akashti30004@gmail.com>
2026-02-06 00:19:46 +0530

    fixed the ESlint error

--- src/Components/MyResume.js
+++ src/Components/MyResume.js
@@ -1,5 +1,5 @@
 import { useGSAP } from "@gsap/react";
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 
